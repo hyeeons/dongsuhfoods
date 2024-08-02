fff1a8e20a849fe8d2134472606cfb5ef5d66c38
hyeeons <qouop0101@gmail.com>
2024-08-02 22:28:25 +0900

    헤더 속도 수정 & 레이아웃

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -1,15 +1,15 @@
 $(function () {
 	// header
 	$("#header").mouseenter(function () {
-		$(".nav-bg").stop(true, true).slideDown("slow");
-		$(".nav-list-depth2").stop(true, true).slideDown("slow");
+		$(".nav-bg").stop(true, true).slideDown(200);
+		$(".nav-list-depth2").stop(true, true).slideDown(200);
 		$(".nav-list-depth1 > a").css("color", "#333333")
 		$(".hd-wrap").css("border-bottom", "1px solid #c3c3c3")
 	})
 
 	$("#header").mouseleave(function () {
-		$(".nav-bg").stop(true, true).slideUp("slow");
-		$(".nav-list-depth2").stop(true, true).slideUp("slow");
+		$(".nav-bg").stop(true, true).slideUp(200);
+		$(".nav-list-depth2").stop(true, true).slideUp(200);
 		$(".nav-list-depth1 > a").css("color", "#ffffff")
 		$(".hd-wrap").css("border-bottom", "none")
 	})
