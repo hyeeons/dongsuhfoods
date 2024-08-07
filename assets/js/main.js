fc0ef12e7fccc6432925d7fcfc2a33707e0dd029
hyeeons <qouop0101@gmail.com>
2024-08-07 17:11:08 +0900

    sec-05 TVCF 스와이퍼작업

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,25 @@
 $(function () {
-  // tab
-  $(".txt-box").on("click", function (event) {
-    event.preventDefault();
-    $(".tab").removeClass("active");
-    $(this).parents(".tab").addClass("active");
-  });
+	// tab
+	$(".txt-box").on("click", function (event) {
+		event.preventDefault();
+		$(".tab").removeClass("active");
+		$(this).parents(".tab").addClass("active");
+	});
 
-  //   sec-05 TVCF
-  $(".cf-img-box").slick({
-    centerMode: true,
-    centerPadding: "60px",
-    slidesToShow: 3,
-    focusOnSelect: true,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: "40px",
-          slidesToShow: 3,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: "40px",
-          slidesToShow: 1,
-        },
-      },
-    ],
-  });
+
 });
+
+var swiper = new Swiper(".swiper", {
+	slidesPerView: 3,
+	spaceBetween: 30,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	autoplay: {
+		delay: 5000,
+		disableOnInteraction: true,
+	},
+	loop: true
+});
+
