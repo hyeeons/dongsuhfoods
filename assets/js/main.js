673e14622ebd5bef90952884abe7db78b54aa2ff
hyeeons <qouop0101@gmail.com>
2024-08-15 19:21:00 +0900

    sec-05 스와이퍼 모바일 작업 중 & 풋터 영역 벗어남 해결

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,15 +17,25 @@ $(function () {
 
 // sec-05 TVCF 스와이퍼
 var swiper = new Swiper(".swiper", {
-	slidesPerView: 3,
-	spaceBetween: 30,
+	slidesPerView: 1,
+	spaceBetween: 10,
 	slidesPerGroup: 1,
 	pagination: {
 		el: ".swiper-pagination",
 		clickable: true,
 	},
 	loop: true,
-
+	breakpoints: {
+		0: {
+			slidesPerView: 1,
+			spaceBetween: 10,
+		},
+		// 화면이 430px 이하일 때
+		430: {
+			slidesPerView: 3,
+			spaceBetween: 30,
+		},
+	}
 });
 
 
