cf935b1047628ac6740b301924c0754e9eaaffc0
hyeeons <qouop0101@gmail.com>
2024-08-19 15:30:36 +0900

    모바일 버전에서 헤더 제이쿼리 작동안되게

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -1,25 +1,39 @@
 $(function () {
   // header
-  $("#header").mouseenter(function () {
-    $(".nav-bg").stop(true, true).slideDown(200);
-    $(".nav-list-depth2").stop(true, true).slideDown(200);
-    $(".nav-list-depth1 > a").css("color", "#333333");
-    $(".hd-wrap").css("border-bottom", "1px solid #c3c3c3");
+  function header(){
+	  if (window.innerWidth >= 430) {
+		  $("#header").mouseenter(function () {
+			$(".nav-bg").stop(true, true).slideDown(200);
+			$(".nav-list-depth2").stop(true, true).slideDown(200);
+			$(".nav-list-depth1 > a").css("color", "#333333");
+			$(".hd-wrap").css("border-bottom", "1px solid #c3c3c3");
+		  });
+		
+		  $("#header").mouseleave(function () {
+			$(".nav-bg").stop(true, true).slideUp(200);
+			$(".nav-list-depth2").stop(true, true).slideUp(200);
+			$(".nav-list-depth1 > a").css("color", "#ffffff");
+			$(".hd-wrap").css("border-bottom", "none");
+		  });
+		
+		  $(".nav-list-depth1 > a").mouseenter(function () {
+			$(this).css("color", "#ef383b");
+		  });
+		  $(".nav-list-depth1 > a").mouseleave(function () {
+			$(this).css("color", "#333333");
+		  });
+	  } else {
+		// 모바일 버전에서는 해제
+		$("#header").off("mouseenter mouseleave");
+		$(".nav-list-depth1 > a").off("mouseenter mouseleave");
+	  }
+  }
+  header();
+  $(window).on("resize", function () {
+    header();
   });
 
-  $("#header").mouseleave(function () {
-    $(".nav-bg").stop(true, true).slideUp(200);
-    $(".nav-list-depth2").stop(true, true).slideUp(200);
-    $(".nav-list-depth1 > a").css("color", "#ffffff");
-    $(".hd-wrap").css("border-bottom", "none");
-  });
 
-  $(".nav-list-depth1 > a").mouseenter(function () {
-    $(this).css("color", "#ef383b");
-  });
-  $(".nav-list-depth1 > a").mouseleave(function () {
-    $(this).css("color", "#333333");
-  });
 });
 
 // aos
